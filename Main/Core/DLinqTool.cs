1d02039ee722165776380bafc9e07adbba0d4265
Seven Chen <chenlong1988.love@163.com>
2016-09-01 22:05:24 +0800

    Chage IDLinqTool namespace

--- Main/Core/DLinqTool.cs
+++ Main/Core/DLinqTool.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Core.LinqTool;
 using Core.LinqTool.LinqExpression;
 using Core.Models;
 
-namespace Core.LinqTool
+namespace Core
 {
     public class DLinqTool : IDLinqTool
     {
